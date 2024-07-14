3fd82c9d4177cba4ee10a23e5e4f7570178c64ca
Grzegorz Grzęda <grzegorz.grzeda@g2labs.pl>
2024-07-14 23:19:55 +0200

    Added documentation and refactored CLI code

--- components/cli/src/cli.c
+++ components/cli/src/cli.c
@@ -28,19 +28,20 @@
 #include <string.h>
 #include "linked-list.h"
 
-#define COMMANDS_INPUT_BUFFER_SIZE (200)
-#define COMMANDS_PARAMETER_MAX_COUNT (10)
-
 const char* HELP_COMMAND_NAME = "help";
 const char* HELP_COMMAND_HELP = "Print available commands";
 
 typedef struct cli {
-    cli_print_t cli_print;
+    cli_print_t print;
     linked_list_t* commands;
     char* buffer;
     size_t buffer_end;
-    char** argv_buffer;
-    int argc;
+    size_t buffer_size;
+    char** parameter_buffer;
+    size_t parameter_buffer_size;
+    int parameter_count;
+    char enter_character;
+    const char* omit_characters;
 } cli_t;
 
 typedef struct cli_entry {
@@ -61,11 +62,11 @@ static int command_help(cli_t* cli, int argc, char** argv) {
             max_cmd_length = size;
         }
     }
-    cli->cli_print("Available commands:\n");
+    cli->print("Available commands:\n");
     for (linked_list_iterator_t* it = linked_list_iterator_begin(cli->commands); it;
          it = linked_list_iterator_next(it)) {
         cli_entry_t* entry = linked_list_get(it);
-        cli->cli_print(" %*s - %s\n", max_cmd_length, entry->name, entry->help);
+        cli->print(" %*s - %s\n", max_cmd_length, entry->name, entry->help);
     }
     return 0;
 }
@@ -90,17 +91,20 @@ static char* get_first_white_character(char* input) {
     return input;
 }
 
-static void parse_arguments(cli_t* cli) {
+static int parse_arguments(cli_t* cli) {
     if (cli->buffer_end < 1) {
-        return;
+        return CLI_RETURN_ERROR_PARAMETER_BUFFER_EMPTY;
     }
-    cli->argc = 0;
+    cli->parameter_count = 0;
     for (char* ptr = cli->buffer; *ptr || (ptr < (cli->buffer + cli->buffer_end));) {
         ptr = get_first_non_white_character(ptr);
         if (*ptr == '\0') {
             break;
         }
-        cli->argv_buffer[cli->argc++] = ptr;
+        if (cli->parameter_count >= cli->parameter_buffer_size) {
+            return CLI_RETURN_ERROR_PARAMETER_COUNT_EXCEEDED;
+        }
+        cli->parameter_buffer[cli->parameter_count++] = ptr;
         ptr = get_first_white_character(ptr);
         if (*ptr == '\0') {
             break;
@@ -108,42 +112,62 @@ static void parse_arguments(cli_t* cli) {
         *(ptr++) = '\0';
     }
     cli->buffer_end = 0;
+    return CLI_RETURN_CONTINUE;
 }
 
 static int execute_command(cli_t* cli) {
-    if (cli->argc < 1) {
-        return -2;
+    if (cli->parameter_count < 1) {
+        return CLI_RETURN_ERROR_COMMAND_NOT_FOUND;
     }
     for (linked_list_iterator_t* it = linked_list_iterator_begin(cli->commands); it;
          it = linked_list_iterator_next(it)) {
         cli_entry_t* entry = linked_list_get(it);
-        if (strcmp(cli->argv_buffer[0], entry->name) == 0) {
-            return entry->command(cli, cli->argc, cli->argv_buffer);
+        if (strcmp(cli->parameter_buffer[0], entry->name) == 0) {
+            return entry->command(cli, cli->parameter_count, cli->parameter_buffer);
         }
     }
     command_help(cli, 0, 0);
-    return -1;
+    return CLI_RETURN_ERROR_COMMAND_NOT_FOUND;
 }
 
-cli_t* cli_create(cli_print_t cli_print) {
+cli_t* cli_create(cli_config_t* config) {
+    if (!config) {
+        return NULL;
+    }
     cli_t* cli = calloc(1, sizeof(cli_t));
     if (!cli) {
         return NULL;
     }
-    cli->buffer = calloc(COMMANDS_INPUT_BUFFER_SIZE, sizeof(char));
+    cli->buffer_size =
+        (config->max_input_buffer_size ? config->max_input_buffer_size : CLI_DEFAULT_MAX_INPUT_BUFFER_SIZE);
+    cli->parameter_buffer_size =
+        (config->max_parameter_count ? config->max_parameter_count : CLI_DEFAULT_MAX_PARAMETER_COUNT);
+    cli->buffer = calloc(cli->buffer_size, sizeof(char));
     if (!cli->buffer) {
         free(cli);
         return NULL;
     }
-    cli->argv_buffer = calloc(COMMANDS_PARAMETER_MAX_COUNT, sizeof(char*));
-    if (!cli->argv_buffer) {
+    cli->parameter_buffer = calloc(cli->parameter_buffer_size, sizeof(char*));
+    if (!cli->parameter_buffer) {
         free(cli->buffer);
         free(cli);
         return NULL;
     }
-    cli->cli_print = cli_print;
+    cli->print = config->print;
+    cli->enter_character = (config->enter_character ? config->enter_character : CLI_DEFAULT_ENTER_CHARACTER);
+    cli->omit_characters = (config->omit_characters ? config->omit_characters : CLI_DEFAULT_OMIT_CHARACTERS);
     cli_register(cli, HELP_COMMAND_NAME, HELP_COMMAND_HELP, command_help);
     return cli;
+}
+
+void cli_destroy(cli_t* cli) {
+    if (!cli) {
+        return;
+    }
+    linked_list_destroy(cli->commands);
+    free(cli->buffer);
+    free(cli->parameter_buffer);
+    free(cli);
 }
 
 void cli_register(cli_t* cli, const char* name, const char* help, cli_command_t command) {
@@ -160,13 +184,20 @@ void cli_register(cli_t* cli, const char* name, const char* help, cli_command_t 
     linked_list_append(cli->commands, entry);
 }
 
-void cli_process(cli_t* cli, char c) {
-    if ((c == '\n') || (c == '\r')) {
+int cli_process(cli_t* cli, char c) {
+    if ((c == cli->enter_character) && (cli->buffer_end > 0)) {
         cli->buffer[cli->buffer_end] = '\0';
-        parse_arguments(cli);
-        execute_command(cli);
+        int parse_result = parse_arguments(cli);
+        if (parse_result != CLI_RETURN_CONTINUE) {
+            return parse_result;
+        }
+        return execute_command(cli);
     } else {
-        cli->buffer[cli->buffer_end] = c;
-        cli->buffer_end++;
+        if (cli->buffer_end < cli->buffer_size) {
+            cli->buffer[cli->buffer_end] = c;
+            cli->buffer_end++;
+            return CLI_RETURN_CONTINUE;
+        }
+        return CLI_RETURN_ERROR_PARAMETER_BUFFER_OVERFLOW;
     }
 }
--- components/cli/src/cli.h
+++ components/cli/src/cli.h
@@ -22,19 +22,80 @@
 #ifndef CLI_H
 #define CLI_H
 
-// TODO: add doxygen comments
+#include <stddef.h>
 
-typedef void (*cli_print_t)(const char* format, ...);
+/**
+ * @defgroup event_handler Event Handler
+ * @brief Simple and efficient C library for handling events
+ * @{
+ */
+#define CLI_DEFAULT_MAX_INPUT_BUFFER_SIZE 200 /**< Default maximum size of input buffer */
+#define CLI_DEFAULT_MAX_PARAMETER_COUNT 10    /**< Default maximum number of parameters */
+#define CLI_DEFAULT_ENTER_CHARACTER '\r'      /**< Default enter character */
+#define CLI_DEFAULT_OMIT_CHARACTERS "\t\n"    /**< Default omit characters */
+
+#define CLI_RETURN_ERROR_COMMAND_NOT_FOUND -1         /**< Command not found */
+#define CLI_RETURN_ERROR_PARAMETER_COUNT_EXCEEDED -2  /**< Exceeded maximum number of parameters */
+#define CLI_RETURN_ERROR_PARAMETER_BUFFER_OVERFLOW -3 /**< Exceeded maximum size of parameter buffer */
+#define CLI_RETURN_ERROR_PARAMETER_BUFFER_EMPTY -4    /**< Parameter buffer is empty */
+#define CLI_RETURN_CONTINUE -5                        /**< Continue processing */
+
+typedef void (*cli_print_t)(const char* format, ...); /**< Print function pointer */
 
 typedef struct cli cli_t;
 
+typedef struct cli_config {
+    cli_print_t print;            /**< Print function */
+    size_t max_input_buffer_size; /**< Maximum input buffer size */
+    size_t max_parameter_count;
+    char enter_character;
+    const char* omit_characters;
+} cli_config_t; /**< CLI configuration structure definition */
+
+/**
+ * @brief CLI Command handler type
+ * @param[in] cli pointer to CLI instance
+ * @param[in] argc number of passed parameters
+ * @param[in] argv parameter buffer
+ * @return integer with command status
+ */
 typedef int (*cli_command_t)(cli_t* cli, int argc, char** argv);
 
-// TODO: add external parametrisation of input and argv buffer sizes
-cli_t* cli_create(cli_print_t cli_print);
+/**
+ * @brief Create CLI instance based of configuration data
+ *
+ * @param[in] config pointer to configuration structure
+ * @return pointer to CLI instance
+ * @return NULL in case of errors
+ */
+cli_t* cli_create(cli_config_t* config);
 
+/**
+ * @brief Destroy a CLI instance
+ * @param[in] cli pointer to the CLI instance
+ */
+void cli_destroy(cli_t* cli);
+
+/**
+ * @brief Register a new command
+ * @param[in] cli pointer to CLI instance
+ * @param[in] name C-Str with command name to display with help command
+ * @param[in] help C-Str with help text to display with help command
+ * @param[in] command pointer to the command handler
+ */
 void cli_register(cli_t* cli, const char* name, const char* help, cli_command_t command);
 
-void cli_process(cli_t* cli, char c);
+/**
+ * @brief Process the CLI with new input character
+ * @param[in] cli pointer to CLI instance
+ * @param[in] c new input character
+ * @return return code from CLI in case of error
+ * @return return code from executed command
+ */
+int cli_process(cli_t* cli, char c);
+
+/**
+ * @}
+ */
 
 #endif  // CLI_H
