12934b2820107d789fd4c5ad91fc57ab3362a99e
Grzegorz Grzęda <grzegorz.grzeda@g2labs.pl>
2024-07-14 16:10:39 +0200

    Added implementation for linked list library

--- components/g2l-linked-list/src/g2l-linked-list.h
+++ components/g2l-linked-list/src/g2l-linked-list.h
@@ -24,6 +24,83 @@
 #ifndef G2L_LINKED_LIST_H
 #define G2L_LINKED_LIST_H
 
-void some_function(void);
+#include <stddef.h>
+
+/**
+ * @defgroup g2l-linked-list G2L Linked list
+ *
+ * @brief A doubly linked list.
+ * @{
+ */
+
+typedef struct g2l_linked_list g2l_linked_list_t;
+typedef struct g2l_linked_list_iterator g2l_linked_list_iterator_t;
+
+/**
+ * @brief Create a new linked list.
+ * @return A pointer to the newly created linked list.
+ * @return NULL if the linked list could not be created.
+ * @note The linked list must be destroyed with g2l_linked_list_destroy() when it is
+ * no longer needed.
+ */
+g2l_linked_list_t* g2l_linked_list_create(void);
+
+/**
+ * @brief Destroy a linked list.
+ * @param list A pointer to the linked list to destroy.
+ * @note This function will free all memory allocated for the linked list.
+ */
+void g2l_linked_list_destroy(g2l_linked_list_t* list);
+
+/**
+ * @brief Append data to the end of a linked list.
+ * @param list A pointer to the linked list.
+ * @param data A pointer to the data to append.
+ */
+void g2l_linked_list_append(g2l_linked_list_t* list, void* data);
+
+/**
+ * @brief Get an iterator to the beginning of a linked list.
+ * @param list A pointer to the linked list.
+ * @return A pointer to the iterator.
+ * @return NULL if the iterator could not be created.
+ */
+g2l_linked_list_iterator_t* g2l_linked_list_iterator_begin(g2l_linked_list_t* list);
+
+/**
+ * @brief Get an iterator to the end of a linked list.
+ * @param list A pointer to the linked list.
+ * @return A pointer to the iterator.
+ * @return NULL if the iterator could not be created.
+ */
+g2l_linked_list_iterator_t* g2l_linked_list_iterator_end(g2l_linked_list_t* list);
+
+/**
+ * @brief Get next iterator in a linked list.
+ * @param iterator A pointer to the iterator.
+ * @return A pointer to the next iterator.
+ * @return NULL if there are no more iterators.
+ */
+g2l_linked_list_iterator_t* g2l_linked_list_iterator_next(g2l_linked_list_iterator_t* iterator);
+
+/**
+ * @brief Get previous iterator in a linked list.
+ * @param iterator A pointer to the iterator.
+ * @return A pointer to the previous iterator.
+ * @return NULL if there are no more iterators.
+ */
+g2l_linked_list_iterator_t* linked_list_iterator_prev(g2l_linked_list_iterator_t* iterator);
+
+/**
+ * @brief Get the next element in a linked list.
+ * @param iterator A pointer to the iterator.
+ * @return A pointer to the next element.
+ * @return NULL if there are no more elements.
+ */
+void* linked_list_get(g2l_linked_list_iterator_t* iterator);
+
+/**
+ * @}
+ */
 
 #endif  // G2L_LINKED_LIST_H
